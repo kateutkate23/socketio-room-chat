0728f3017099c40c6bff1ef08e285cdecec09fbe
bendenko-v <bendenko.v@gmail.com>
2024-07-01 14:19:52 +0200

    Init

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-
 import eventlet
+from eventlet import wsgi
 import socketio
 from loguru import logger
 
-
 ROOMS = ["lobby", "general", "random"]
+
 # Заставляем работать пути к статике
 static_files = {'/': 'static/index.html', '/static': './static'}
 sio = socketio.Server(cors_allowed_origins='*', async_mode='eventlet')
 app = socketio.WSGIApp(sio, static_files=static_files)
+
 
 # Обрабатываем подключение пользователя
 @sio.event
 def connect(sid, environ):
     logger.info(f"Пользователь {sid} подключился")
 
+
 # Обрабатываем запрос очерендного вопроса
 @sio.on('get_rooms')
 def on_get_rooms(sid, data):
-   pass
+    pass
+
 
 # Обрабатывем отправку ответа
 @sio.on('message')
@@ -35,7 +38,8 @@ def on_leave(sid, data):
 # Обрабатываем отключение пользователя
 @sio.event
 def disconnect(sid):
-     logger.info(f"Пользователь {sid} отключился")
+    logger.info(f"Пользователь {sid} отключился")
 
 
-eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 80)), app)
+if __name__ == '__main__':
+    wsgi.server(eventlet.listen(("127.0.0.1", 8000)), app)
